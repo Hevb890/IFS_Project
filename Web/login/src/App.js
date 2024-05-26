05932e03f520c0c8de20fe78fa2f13fcba9579a4
sharhaan <sharhaanmfm@gmail.com>
2024-05-26 15:50:45 +0530

    chore: Update package-lock.json and package.json files

--- Web/login/src/App.js
+++ Web/login/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5f4e49a9b0366c632ad999fe0ba4a6fd8be25295d45399ee2d764bd2121ce6d
-size 206
+oid sha256:ef35cde356099abb63682e53df509903b72e2e9decddccc0e6250e6934e919e2
+size 252
---
+++ Web/login/src/forget.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:57159896fec559accf0b587e4a9304ef8a025ca4f99a9d12d9c9e57f0ec737bf
+size 7022
