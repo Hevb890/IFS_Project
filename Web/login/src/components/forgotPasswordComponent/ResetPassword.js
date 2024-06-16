1dc460997985f4b1600f9fe6d8d81e0dc0ce9b7d
sharhaan <sharhaanmfm@gmail.com>
2024-06-17 01:48:29 +0530

    from sharhaan

---
+++ Web/login/src/components/forgotPasswordComponent/ResetPassword.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:21b1af5770e8e8a8af449c602789b8ea6ff05b831e90ef4f6212415195a10e75
+size 4119
---
+++ Web/login/src/components/forgotPasswordComponent/otpverificationPage.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bb13ba2658a286ba3be64ab70a1a31312488739b7aa1c42c26c454307a72159c
+size 3177
